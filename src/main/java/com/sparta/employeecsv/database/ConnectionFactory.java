822fee733a1346176f9f28c9e7dbeca1ea75ed42
Alex Costi <alex_costi@icloud.com>
2022-04-26 16:14:28 +0100

    connectionfactory

---
+++ src/main/java/com/sparta/employeecsv/database/ConnectionFactory.java
@@ -0,0 +1,14 @@
+package com.sparta.employeecsv.database;
+
+import java.sql.Connection;
+
+public class ConnectionFactory {
+
+    private static Connection connection = null;
+
+    private ConnectionFactory() {}
+
+    public 
+
+
+}
