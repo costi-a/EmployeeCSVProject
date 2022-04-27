b86a5903b1be421e7e92d867682a1b1cee58f48a
Alex Costi <alex_costi@icloud.com>
2022-04-27 14:08:06 +0100

    clear table

--- src/main/java/com/sparta/employeecsv/database/DatabaseDriver.java
+++ src/main/java/com/sparta/employeecsv/database/DatabaseDriver.java
@@ -2,11 +2,15 @@ package com.sparta.employeecsv.database;
 
 import com.sparta.employeecsv.model.Employee;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
+import java.util.Properties;
 
 public class DatabaseDriver {
     private Connection connection;
@@ -15,7 +19,7 @@ public class DatabaseDriver {
         connection = ConnectionFactory.getConnection();
     }
 
-    public void createTable()   {
+    public void createTable() {
         try {
             //create the employee list table in the database
             String createTable = "Create Table EMPLOYEE-RECORDS " +
@@ -25,9 +29,9 @@ public class DatabaseDriver {
                     "MiddleInitial CHAR(1)," +
                     "LastName VARCHAR(25)," +
                     "Gender CHAR(1)," +
-                    "Email VARCHAR(50),"  +
-                    "DateOfBirth DATE,"   +
-                    "DateOfJoining DATE,"  +
+                    "Email VARCHAR(50)," +
+                    "DateOfBirth DATE," +
+                    "DateOfJoining DATE," +
                     "Salary DECIMAL(10,2)," +
                     "PRIMARY KEY (EmployeeID)" +
                     ");";
@@ -41,16 +45,11 @@ public class DatabaseDriver {
     }
 
     public void populateTable(List<Employee> employeeList) {
-
-        String dbInsert = "INSERT INTO EMPLOYEE-RECORDS " +
-                "(EmployeeID, NamePrefix, FirstName, MiddleInitial, LastName, " +
-                "Gender, Email, DateOfBirth, DateOfJoining, Salary)" +
-                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-
+        //for each employee in the list get their details and add it to the database
         try {
-            PreparedStatement ps = connection.prepareStatement(dbInsert);
+            PreparedStatement ps = connection.prepareStatement(getInsertSQL());
 
-            for (Employee employee : employeeList)  {
+            for (Employee employee : employeeList) {
                 ps.setString(1, employee.getEmployeeID());
                 ps.setString(2, employee.getNamePrefix());
                 ps.setString(3, employee.getFirstName());
@@ -69,5 +68,33 @@ public class DatabaseDriver {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public void clearTable()    {
+        //drop the table from the database
+
+        String drop = "DROP TABLE [IF EXISTS] EMPLOYEE-RECORDS";
+        try {
+            Statement st = connection.createStatement();
+            st.executeUpdate(drop);
+            st.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    
+
+    private String getInsertSQL() {
+        //get the sql insert property from the properties file
+        Properties sqlProps = new Properties();
+        try {
+            sqlProps.load(new FileReader("sql.properties"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sqlProps.getProperty("db.sql-insert");
     }
 }
